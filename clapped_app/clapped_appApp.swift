e9dc0a9c71d18b532c4cff5afa3f04c7ee27a974
shunnarski <ashunnarah9597@gmail.com>
2021-02-06 12:50:18 -0600

    Initial Commit

---
+++ clapped_app/clapped_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  clapped_appApp.swift
+//  clapped_app
+//
+//  Created by Alec Shunnarah on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct clapped_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
